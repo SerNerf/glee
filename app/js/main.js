99d3003f6d3d91af682582cd77845490f9370ce2
SerNerf <shaydurov2000@mail.ru>
2021-01-31 17:41:45 +0300

    Подключил mixitup, интегрировал в код

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,11 @@
 $(function(){
-
+  
   $('.main-slider__inner').slick({
     arrows: false,
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
   })
-
+  
+  var mixer = mixitup('.filter__wrapper');
 });
