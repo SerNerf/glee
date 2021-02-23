3e6539c1fdb1a5b0bd9c0a110ae0dbd77106e86e
SerNerf <shaydurov2000@yandex.ru>
2021-02-23 15:55:20 +0300

    textgit

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,14 @@ $(function(){
     slidesToShow: 1,
     slidesToScroll: 1,
   })
+
+  $('.sponsors__items').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+  })
   
   var mixer = mixitup('.products-week__wrapper');
 });
