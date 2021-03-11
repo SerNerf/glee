95da578c0500445d3f4de8291dfdd888346a261c
SerNerf <shaydurov2000@yandex.ru>
2021-03-11 11:49:14 +0300

    Сделал адаптив шапки, breadcrumbs и about

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
 $(function(){
   
+  // $(document).click(function(event) {
+  //   if($(window).width() <= 575){
+  //       //Скрыть меню на смартфоне при клике вне него
+  //       if (
+  //            !($('.menu__items').find(event.target).length 
+  //            || $('.menu__items')[0] == $(event.target)[0] 
+  //            || $('.menu__btn').find(event.target).length)
+             
+  //            && $('.menu__items').hasClass('menu__items--active')
+  //            && $('.calculator-window--eror').hasClass('hide')
+  //            && !$(event.target).hasClass('menu__btn')
+  //            ){
+  //           $('.menu__items').removeClass('menu__items--active');
+  //           $('.menu__line').removeClass('menu__line--active');
+  //       }
+  //   }
+  // });
+  
+  $('.user-nav__link--menu').click(function(){
+    $('.menu').toggleClass('menu--active');
+  })
+
   $('.main-slider__inner').slick({
     arrows: false,
     dots: true,
